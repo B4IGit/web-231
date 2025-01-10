bbc090665dab10816c142e041c32893ab60a1d00
Devin Ledesma <dev.dfledesma@gmail.com>
2025-01-09 20:26:41 -0600

    defined variables for service name and service speed

--- week-1/project01-02/project01-02_txt.js
+++ week-1/project01-02/project01-02_txt.js
@@ -2,10 +2,19 @@
     Chapter 1
     Hands-On Project 1-2
 
-    Author: 
-    Date:   
+    Author: Devin Ledesma
+    Date:   01/09/2025
 
     Filename: project01-02.js
 */
 
 //define variables for service name and service speed
+const service1Name = "Basic";
+const service2Name = "Express";
+const service3Name = "Extreme";
+const service4Name = "Ultimate";
+
+const service1Speed = "50 Mbsp";
+const service2Speed = "100 Mbsp";
+const service3Speed = "500 Mbsp";
+const service4Speed = "1 Gig";
