354e0ce345e2ad6959bb42ca1a790d3272645222
Devin Ledesma <dev.dfledesma@gmail.com>
2025-01-16 22:43:40 -0600

    created function, verifyForm, to indicate whether all fields have been completed. Used ternary operator to check if statement is truthy or falsy

--- week-2/project02-02/project02-02_txt.js
+++ week-2/project02-02/project02-02_txt.js
@@ -3,9 +3,28 @@
       Project 02-02
 
       Application to test for completed form
-      Author: 
-      Date:   
+      Author: Devin Ledesma
+      Date:   01/16/2025
 
       Filename: project02-02.js
  */
- 
+
+// Function to validate form inputs
+function verifyForm() {
+  // Declares global variables and passes the values submitted from the form
+  var name = document.getElementById('name').value;
+  var email = document.getElementById('email').value;
+  var phone = document.getElementById('phone').value;
+
+  console.log(name);
+  console.log(email);
+  console.log(phone);
+
+  // Conditional Operator to test if data has been entered into inputs
+  name && email && phone
+    ? window.alert('Thank you!') // all fields have been entered
+    : window.alert('Please fill in all fields'); // contains any empty fields
+}
+
+// Handles form submission
+document.getElementById('submit').addEventListener('click', verifyForm, false);
